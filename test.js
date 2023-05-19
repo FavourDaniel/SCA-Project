f0360e1edf8cfca39f7643bae3e63c9300ce735c
Favour Daniel <favourd205@gmail.com>
2023-05-19 13:20:32 +0100

    built cicd pipeline

--- test.js
+++ test.js
@@ -1,8 +1,34 @@
-const request = require('supertest');
+const http = require('http');
 const app = require('./server');
 
-describe('GET /', () => {
-  it('respond with Hello World', (done) => {
-    request(app).get('/').expect('Hello World', done);
+describe('Server', () => {
+  let server;
+
+  before((done) => {
+    server = app.listen(80, done);
+  });
+
+  after((done) => {
+    server.close(done);
+  });
+
+  it('GET / should return status 200', (done) => {
+    http.get('http://localhost:80', (res) => {
+      if (res.statusCode === 200) {
+        done();
+      } else {
+        done(new Error(`Expected status code 200 but received ${res.statusCode}`));
+      }
+    });
+  });
+
+  it('GET /exists should return status 200', (done) => {
+    http.get('http://localhost:80/exists', (res) => {
+      if (res.statusCode === 200) {
+        done();
+      } else {
+        done(new Error(`Expected status code 200 but received ${res.statusCode}`));
+      }
+    });
   });
 });
